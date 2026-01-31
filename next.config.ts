1ca92d9a0fa19125b56ba21a584ae60d03ef3936
Kelly Bell <kbell109@gmail.com>
2026-01-31 18:35:53 -0500

    try static again

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Enables static HTML export
+
   async headers() {
     return [
       {
